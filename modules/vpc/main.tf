01d99d1cfcf11c36674cb962e35ca784c1e88f65
Arpita Patnaik <32553590+arpita497@users.noreply.github.com>
2023-03-23 14:31:25 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -8,7 +8,7 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
   version = "3.19.0"
 
-  name = "terraform-vpc"
+  name = "terraform1-vpc"
   cidr = "10.0.0.0/16"
 
   azs             = ["ap-south-1a", "ap-south-1b"]
